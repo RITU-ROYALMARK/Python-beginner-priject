c7594f58773c9c6217003f386a7eab81cc29b0b6
RITU-ROYALMARK <161790423+RITU-ROYALMARK@users.noreply.github.com>
2024-03-02 18:15:55 +0530

    Add files via upload
    SO THIS PROJECT  ITS GOING TO SWAP THE EMPTY SPACE WITH (-) SPLASH EXAMPLE:-
     INPUT:-
          MY NAME IS RITHIN MP
    OUTPUT:- 
           MY-NAME-IS-RITHIN-MP

---
+++ SWAP_CASE.py
@@ -0,0 +1,12 @@
+# SWAPE CASE
+
+
+PLAYER = input("enter the word: ")
+
+# so its going to swap the empty space with - slash by using replace function
+
+swap = PLAYER.replace(" ","-")
+
+#print swap
+
+print(swap)
