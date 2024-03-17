51ae6e71b80b22a58be258cf8ab29296461d8166
RITU-ROYALMARK <161790423+RITU-ROYALMARK@users.noreply.github.com>
2024-03-17 07:20:40 +0530

    Algorithm to add two numbers

---
+++ Alogrithim_to_add_two_number.py
@@ -0,0 +1,13 @@
+# algortihms to add two number
+
+class add:
+    def __init__(self, val, val1):
+        self.val = val
+        self.val1 = val1
+
+    def add_two_number(self):
+        return self.val + self.val1
+
+
+a = add(20, 20)
+print(a.add_two_number())
